b13517a87cd6df622b9bbf6a978315043e6ec8a1
MKG214 <mkgamer214@gmail.com>
2023-08-24 12:55:39 -0700

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,23 @@
-#include <stdio.h>
 #include "lists.h"
 
 /**
+ * _strlen - return the lenght of a string
+ * @s: the string whose lenght to check s 
+ *
+ * Return: the number of nodes printed
+ */
+int _strlen(char *s)
+{
+  int i =0;
+
+  if (!s)
+    return (0);
+  while (*s++)
+    i++;
+  return (i);
+}
+
+ /**
  * print_list - prints all the elements of a linked list
  * @h: pointer to the list_t list to print
  *
@@ -9,7 +25,7 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t s = 0;
+	size_t i = 0;
 
 	while (h)
 	{
